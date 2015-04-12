63e6a8550ba771bb43a405b01704f19da5f7cc9f
Alexandre Machado <alexandre@machado.cc>
2015-04-12 10:10:50 -0300

    implementa distÂncias das paradas

--- dotnet.api/Models/ParadaOnibus.cs
+++ dotnet.api/Models/ParadaOnibus.cs
@@ -5,12 +5,10 @@ using System.Web;
 
 namespace dotnet.api.Models
 {
-    public class ParadaModel
+    public class ParadaOnibus
     {
         public int ParadaID { get; set; }
 
-        public double Latitute { get; set; }
-
-        public double Longitude { get; set; }
+        public int OnibusID { get; set; }
     }
 }
